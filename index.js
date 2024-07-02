bfd45d05b195e433e049de0cc75d4e1b85ab5563
Aashi19234 <aasthaj375@gmail.com>
2024-07-02 16:51:44 +0530

    github data added

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const app = express()// express ko use krke app variable bna lia
 const port = 4000
 //server listens through port
 
+const githubdata={
+  "login": "Aashi19234",
+  "id": 140010647,
+  "node_id": "U_kgDOCFhklw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/140010647?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Aashi19234",
+  "html_url": "https://github.com/Aashi19234",
+  "followers_url": "https://api.github.com/users/Aashi19234/followers",
+  "following_url": "https://api.github.com/users/Aashi19234/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Aashi19234/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Aashi19234/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Aashi19234/subscriptions",
+  "organizations_url": "https://api.github.com/users/Aashi19234/orgs",
+  "repos_url": "https://api.github.com/users/Aashi19234/repos",
+  "events_url": "https://api.github.com/users/Aashi19234/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Aashi19234/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Aastha Jain",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "Student at Bhagwan Parshuram Institute Of Technology | Cse’26 | C| C++| Beginner in DSA | Front End Developer",
+  "twitter_username": null,
+  "public_repos": 34,
+  "public_gists": 0,
+  "followers": 6,
+  "following": 5,
+  "created_at": "2023-07-19T18:40:26Z",
+  "updated_at": "2024-06-20T10:35:54Z"
+}
+
 app.get('/', (req, res) => {// app ek get request krra h
   res.send('Hello World!')// response m send krdo hello world
 
@@ -27,7 +62,9 @@ app.get('/login',(req,res)=>{
 app.get('/youtube',(req,res)=>{
     res.send('<h2> Chai aur code</h2>')
 })
-
+app.get('/github',(req,res)=>{
+  res.json(githubdata)
+})
 // yaha pr listen krenge through app as it is made from express
 app.listen(process.env.PORT, () => {// on successfully listening console.log statement will be printed
 
@@ -81,6 +118,20 @@ How to deploy an appication?
 
 steps to deploy:
 1) push project on github
+2) go to digital ocean
+3) click on create and then app
+4) upload repository
+5) write main branch
+6) click on next
+7) pay money
+8) go to environment variables
+7) add ur env variable PORT and its value 
+8) yaha pr agr unpe 3000 ka port availabe ni hua toh unpe jo
+hga availabe vo usse overwrite krdenge 3000 ko
+9) review and click create resource
+10) go to build log
+11) Apps m url mil jaega
+
 
 
 
